d77fc7bcc0b9ac6e71e0bc7e821ab9fc8baca6e4
Malik Fassi <malikfassifihri@gmail.com>
2024-11-11 15:05:29 +0100

    feat: implement tabbed navigation for game view and dev panel

--- src/app.rs
+++ src/app.rs
@@ -1,20 +1,12 @@
 use yew::prelude::*;
 use crate::components::{DevPanel, GameView, State};
-use crate::hooks::{{use_game_state, GameStateHandle}};
+use crate::hooks::{use_game_state, GameStateHandle};
 use crate::predefined_states::load_predefined_states;
 
 #[function_component(App)]
 pub fn app() -> Html {
     let GameStateHandle { state, on_action } = use_game_state();
     let predefined_states = use_state(|| load_predefined_states());
-    let active_tab = use_state(|| "GameView".to_string());
-
-    let switch_tab = {
-        let active_tab = active_tab.clone();
-        Callback::from(move |tab: String| {
-            active_tab.set(tab);
-        })
-    };
 
     let on_select_predefined_state = {
         let state = state.clone();
@@ -32,18 +24,19 @@ pub fn app() -> Html {
                 <State state={state.clone()} on_select_predefined_state={on_select_predefined_state.clone()} />
             </div>
             <div class="main-content">
-                <div class="navbar">
-                    <button onclick={switch_tab.reform(|_| "GameView".to_string())}>{ "Game View" }</button>
-                    <button onclick={switch_tab.reform(|_| "DevPanel".to_string())}>{ "Dev Panel" }</button>
-                </div>
-                <div class="content">
-                    {
-                        if *active_tab == "GameView" {
-                            html! { <GameView state={state.clone()} on_action={on_action.clone()} /> }
-                        } else {
-                            html! { <DevPanel game_state={state.clone()} on_parameter_change={on_action.clone()} /> }
-                        }
-                    }
+                <div class="tabs">
+                    <div class="tab">
+                        <h2>{ "Game View" }</h2>
+                        <GameView state={state.clone()} on_action={on_action.clone()} />
+                    </div>
+                    <div class="tab">
+                        <h2>{ "Dev Panel" }</h2>
+                        <DevPanel game_state={state.clone()} on_parameter_change={on_action.clone()} />
+                    </div>
+                    <div class="tab">
+                        <h2>{ "State Management" }</h2>
+                        <State state={state.clone()} on_select_predefined_state={on_select_predefined_state.clone()} />
+                    </div>
                 </div>
             </div>
         </div>
